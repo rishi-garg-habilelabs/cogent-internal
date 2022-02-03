f738285e636a8f3c67af63bc60799e73481558e8
rekhabanga <rekha.banga@habilelabs.io>
2022-02-03 13:53:45 +0530

    api call

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -4,32 +4,31 @@ import { useTranslation } from "react-i18next";
 import CardContent from "@mui/material/CardContent";
 import { Box, Typography } from "@mui/material";
 import Button from "../../components/atoms/button";
-import {Buttons}  from "../../constant";
+import { Buttons } from "../../constant";
 import { useNavigate } from "react-router-dom";
 import { getData } from "../../store/services/auth";
 import useStyles from "../../custom-hooks/useStyles";
 import styles from "./style";
-import { httpservice } from "../../react-config/httpservice";
 
-
-const HomePage=() =>{
+const HomePage = () => {
   const { t } = useTranslation();
   const classes = useStyles(styles)();
   const navigate = useNavigate();
-  const getDataApi = async ()=>{
+  // make service and  call api using respective service
+  const getDataApi = async () => {
     // const payload = {};
-   const response =  await getData()
-   console.log('response',response)
-  }
-  React.useEffect(()=>{
-    getDataApi()
-  },[])
+    //  const response =  await getData()
+    //  console.log('response',response)
+  };
+  React.useEffect(() => {
+    getDataApi();
+  }, []);
   return (
     <Box display="flex" className={classes.wrapper}>
       <Box
         display="flex"
         sx={{
-          width: '25%',
+          width: "25%",
           height: 300,
         }}
       >
@@ -40,7 +39,7 @@ const HomePage=() =>{
                 {t("RETURN")}
               </Typography>
             </Box>
-            <Box  className={classes.textMargin}>
+            <Box className={classes.textMargin}>
               <Typography
                 sx={{ fontSize: 14 }}
                 color="text.primary"
@@ -53,13 +52,13 @@ const HomePage=() =>{
             <Box className={classes.textMargin}>
               <Button btntype={Buttons.PRIMARY} name={t("SIGN_IN")} />
             </Box>
-            <Box  className={classes.textMargin}>
+            <Box className={classes.textMargin}>
               <Typography sx={{ mb: 1.5 }} color="text.secondary">
                 {t("PROBLIEM_IN_SIGN_IN")}
               </Typography>
             </Box>
             <Box className={classes.textMargin}>
-              <Box  className={classes.textMargin}>
+              <Box className={classes.textMargin}>
                 <Button btntype={Buttons.SECONDARY} name="RECOVER ACCOUNT" />
               </Box>
               <Box className={classes.textMargin}>
@@ -72,19 +71,18 @@ const HomePage=() =>{
       <Box
         display="flex"
         sx={{
-          width: '25%',
+          width: "25%",
           height: 300,
         }}
       >
-        
         <Card className={classes.cardRight} sx={{ maxWidth: 400 }}>
           <CardContent>
             <Box style={{ marginTop: 5 }}>
-              <Typography sx={{ fontSize: 18, fontWeight: 500}}>
+              <Typography sx={{ fontSize: 18, fontWeight: 500 }}>
                 {t("NEW_REGISTER")}
               </Typography>
             </Box>
-            <Box  className={classes.textMargin}>
+            <Box className={classes.textMargin}>
               <Typography sx={{ fontSize: 14 }}>
                 {t("FIRST_TIME_PARENT")}
               </Typography>
@@ -96,13 +94,13 @@ const HomePage=() =>{
                 onClick={() => navigate("/register")}
               />
             </Box>
-            <Box  className={classes.textMargin}>
+            <Box className={classes.textMargin}>
               <Typography>{t("RETURN_STUDENT")}</Typography>
             </Box>
             <Box className={classes.textMargin}>
               <Button btntype={Buttons.SECONDARY} name={t("WATCH_VIDEO")} />
             </Box>
-            <Box  className={classes.textMargin}>
+            <Box className={classes.textMargin}>
               <Typography variant="body2">{t("JOIN_BACK")}</Typography>
             </Box>
           </CardContent>
@@ -110,6 +108,6 @@ const HomePage=() =>{
       </Box>
     </Box>
   );
-}
+};
 
-export default HomePage
+export default HomePage;
