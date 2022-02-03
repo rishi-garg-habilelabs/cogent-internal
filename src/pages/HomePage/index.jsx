916a805a54a890c015039e5223958a4fd77fe2ec
rekhabanga <rekha.banga@habilelabs.io>
2022-02-03 11:02:38 +0530

    home page modified

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -9,82 +9,95 @@ import Button from "../../components/atoms/button";
 import BTN from "../../constant/buttonTypes";
 import { useNavigate } from "react-router-dom";
 
-import useStyles from '../../custom-hooks/useStyles'
-import styles from './style'
+import useStyles from "../../custom-hooks/useStyles";
+import styles from "./style";
 
 export default function HomePage() {
-    const classes = useStyles(styles)()
-    const navigate =useNavigate()
+  const classes = useStyles(styles)();
+  const navigate = useNavigate();
   return (
-        
-    <Grid container justifyContent="center" className={classes.wrapper}> 
-    <Grid container justifyContent="center" className={classes.mainContainer}> 
-      <Grid item xs={6}>
-        <Card  className={classes.cardLeft} sx={{ maxWidth: 400 }}>
-          <CardContent>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.secondary"
-              gutterBottom
-            >
-              Returning Parents/ Teacher/ Coordinators
-            </Typography>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.primary"
-              gutterBottom
-              component="div"
-            >
-              If you have your Manabadi Google Platform (Classroom) User ID and
-              Password, please sign in.
-            </Typography>
-            <Button btntype={BTN.PRIMARY} name="SIGN IN TO MANABADI PORTAL" />
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              Problem signing in to your Account.
-            </Typography>
-            <Typography variant="body2">
-              If you don't have/remember your Manabadi credentials please use
-              Recovery button to recover it.
-              <Button btntype={BTN.SECONDARY} name="RECOVER ACCOUNT" />
-            </Typography>
-          </CardContent>
-          <CardActions></CardActions>
-        </Card>
+    <Grid container justifyContent="center" className={classes.wrapper}>
+      <Grid container justifyContent="center" className={classes.mainContainer}>
+        <Grid item xs={6}>
+          <Card className={classes.cardLeft} sx={{ maxWidth: 400 }}>
+            <CardContent>
+              <Grid item style={{ marginTop: 5 }}>
+                <Typography sx={{ fontSize: 18, fontWeight: 500 }}>
+                  Returning Parents/ Teacher/ Coordinators
+                </Typography>
+              </Grid>
+              <Grid item className={classes.textMargin}>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.primary"
+                  gutterBottom
+                  component="div"
+                >
+                  If you have your Manabadi Google Platform (Classroom) User ID
+                  and Password, please sign in.
+                </Typography>
+              </Grid>
+              <Grid item className={classes.textMargin}>
+                <Button
+                  btntype={BTN.PRIMARY}
+                  name="SIGN IN TO MANABADI PORTAL"
+                />
+              </Grid>
+              <Grid item className={classes.textMargin}>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  Problem signing in to your Account.
+                </Typography>
+              </Grid>
+              <Grid item className={classes.textMargin}>
+                <Grid className={classes.textMargin}>
+                  <Button btntype={BTN.SECONDARY} name="RECOVER ACCOUNT" />
+                </Grid>
+                <Grid className={classes.textMargin}>
+                  <Typography variant="body2">
+                    If you don't have/remember your Manabadi credentials please
+                    use Recovery button to recover it.
+                  </Typography>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid item xs={6}>
+          <Card className={classes.cardRight} sx={{ maxWidth: 400 }}>
+            <CardContent>
+              <Grid style={{ marginTop: 5 }}>
+                <Typography sx={{ fontSize: 18, fontWeight: 500 }}>
+                  New Registrations
+                </Typography>
+              </Grid>
+              <Grid item className={classes.textMargin}>
+                <Typography sx={{ fontSize: 14 }}>
+                  Not yet registered to the portal (First Time Parents)?
+                </Typography>
+              </Grid>
+              <Grid item className={classes.textMargin}>
+                <Button
+                  btntype={BTN.SECONDARY}
+                  name="REGISTER NOW"
+                  onClick={() => navigate("/register")}
+                />
+              </Grid>
+              <Grid item className={classes.textMargin}>
+                <Typography>Are you a Returning Student?</Typography>
+              </Grid>
+              <Grid item className={classes.textMargin}>
+                <Button btntype={BTN.SECONDARY} name="WATCH VIDEO" />
+              </Grid>
+              <Grid item className={classes.textMargin}>
+                <Typography variant="body2">
+                  All the students who have been assorted to Manabadi and are
+                  joining back will be considered as returning students.
+                </Typography>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
-      <Grid item xs={6}>
-        <Card  className={classes.cardRight} sx={{ maxWidth: 400 }}>
-          <CardContent>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.secondary"
-              gutterBottom
-            >
-              New Registrations
-            </Typography>
-            <Typography
-              sx={{ fontSize: 14 }}
-              color="text.primary"
-              gutterBottom
-              component="div"
-            >
-              Not yet registered to the portal (First Time Parents)?
-            </Typography>
-            <Button btntype={BTN.SECONDARY} name="REGISTER NOW"  onClick={()=>navigate('/register')}/>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              Are you a Returning Student?
-            </Typography>
-            <Button btntype={BTN.SECONDARY} name="WATCH VIDEO" />
-            <Typography variant="body2">
-              All the students who have been assorted to Manabadi and are
-              joining back will be considered as returning students.
-              <br />
-            </Typography>
-           
-          </CardContent>
-          <CardActions></CardActions>
-        </Card>
-      </Grid>
-    </Grid>
     </Grid>
   );
 }
--- src/pages/HomePage/style.js
+++ src/pages/HomePage/style.js
@@ -1,26 +1,29 @@
 const homePageStyle = (theme) => ({
-    mainContainer:{
-        width:900,
-        justifyContent:'center'
-    },
-    wrapper:{
-        justifyContent:'center',
-        marginTop:"10%"
-    },
-    cardLeft: {
-            boxShadow: '0 0px 15px 3px hsl(0deg 0% 65% / 25%)',
-            minHeight:450,
-            textAlign:'center',
-    },
-    cardRight: {
-          background: '#FF6D00',
-          color:'white',
-          textAlign:'center',
-          boxShadow: '0 0px 15px 3px hsl(0deg 0% 65% / 25%)',
-          minHeight:450
-      },
-
-
+  mainContainer: {
+    width: 900,
+    justifyContent: "center",
+  },
+  textMargin:{
+    marginTop: 20 
+  },
+  wrapper: {
+    justifyContent: "center",
+    marginTop: "10%",
+  },
+  cardLeft: {
+    borderRadius: 0,
+    boxShadow: "0 0px 15px 3px hsl(0deg 0% 65% / 25%)",
+    minHeight: 450,
+    textAlign: "center",
+  },
+  cardRight: {
+    borderRadius: 0,
+    background: "#FF6D00",
+    color: "white",
+    textAlign: "center",
+    boxShadow: "0 0px 15px 3px hsl(0deg 0% 65% / 25%)",
+    minHeight: 450,
+  },
 });
 
 export default homePageStyle;
