db1ed5f37513b86fa850abb02caa9a37af61a8ee
rekhabanga <rekha.banga@habilelabs.io>
2022-02-08 14:37:10 +0530

    fix:extra code removed

--- src/pages/register/parent-info/index.jsx
+++ src/pages/register/parent-info/index.jsx
@@ -1,17 +1,12 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { Card, Grid, CardContent } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import _ from "lodash";
 import style from "./style";
-import TextInput from "../../../components/atoms/textfield";
-// import { auth } from "../../../services/auth";
 import Button from "../../../components/atoms/button";
 import { Buttons } from "../../../constant";
 import FormBuilder from "../../../components/molecules/form";
-import { getUserLocale } from "../../../utils/localStorageMethod";
 import { checkFormValid } from "../../../utils/validations";
-import { handleSnackBar } from "../../../utils/methods";
-import { AuthContext } from "../../../contexts/AuthProvider";
 import { useTranslation } from "react-i18next";
 import constant from "../../../constant/config";
 const ParentInfo = ({ fields }) => {
@@ -19,7 +14,6 @@ const ParentInfo = ({ fields }) => {
   const { t } = useTranslation();
   const classes = style();
   const [formFields, setFormFields] = useState(fields);
-  const { setLoading, setSnackbarConfig } = useContext(AuthContext);
   const [adressErrorCheckBox, setAdressErrorCheckBox] = useState({
     show: false,
     checked: false,
@@ -28,62 +22,6 @@ const ParentInfo = ({ fields }) => {
     if (buttonKey === "save") {
       const result = checkFormValid(formFields);
       setFormFields(result.fields);
-      if (result.isFormValid) {
-        if (!adressErrorCheckBox.checked && constant.verifyAddress) {
-          const countryValue = _.find(
-            fields,
-            (field) => field.key === "country"
-          );
-          const postCodeValue = _.find(
-            fields,
-            (field) => field.key === "postCode"
-          );
-          const streetValue = _.find(fields, (field) => field.key === "street");
-          const streetNumberValue = _.find(
-            fields,
-            (field) => field.key === "number"
-          );
-          const cityValue = _.find(fields, (field) => field.key === "city");
-          // const addressValid = isAddressValid({
-          //   country: [countryValue.value],
-          //   postalCode: [postCodeValue.value],
-          //   addressLines: [
-          //     streetValue.value,
-          //     streetNumberValue.value,
-          //     cityValue.value,
-          //   ],
-          // });
-          // if (!addressValid) {
-          //   setAdressErrorCheckBox({
-          //     ...adressErrorCheckBox,
-          //     show: true,
-          //     checked: false,
-          //   });
-          //   return;
-          // }
-        }
-        //   const payload = getPayloadObj(result.fields);
-        //   try {
-        //     setLoading(true);
-        //     const response = await auth.register({
-        //       ...payload,
-        //       role: "PATIENT",
-        //       lang: getUserLocale(),
-        //     });
-        //     if (response.data.message) {
-        //       setLoading(false);
-        //       setSnackbarConfig(
-        //         handleSnackBar("fire", t("REGISTRATION_SUCCESSFUL"), "success")
-        //       );
-        //       navigate("/login");
-        //     }
-        //   } catch (error) {
-        //     setLoading(false);
-        //     setSnackbarConfig(
-        //       handleSnackBar("fire", error.error || "Api error", "error")
-        //     );
-        //   }
-      }
     } else if (buttonKey === "cancel") {
       navigate("/login");
     }
@@ -94,10 +32,6 @@ const ParentInfo = ({ fields }) => {
     updatedFormFields[index].value = value;
     setFormFields(updatedFormFields);
   };
-  // calling api
-
-  // const payload = {};
-  // const response = await auth.register({ ...payload});
   return (
     <Card>
       <CardContent>
--- src/pages/register/student-info/student-info.jsx
+++ src/pages/register/student-info/student-info.jsx
@@ -1,25 +1,18 @@
-import React, { useState, useContext } from "react";
+import React, { useState } from "react";
 import { Card, Grid, CardContent } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import _ from "lodash";
 import style from "./style";
-import TextInput from "../../../components/atoms/textfield";
-import { auth } from "../../../store/services/auth";
 import Button from "../../../components/atoms/button";
 import { Buttons } from "../../../constant";
 import FormBuilder from "../../../components/molecules/form";
-import { getUserLocale } from "../../../utils/localStorageMethod";
 import { checkFormValid } from "../../../utils/validations";
-import { handleSnackBar } from "../../../utils/methods";
-import { AuthContext } from "../../../contexts/AuthProvider";
 import { useTranslation } from "react-i18next";
-import constant from "../../../constant/config";
 const StudentInfo = ({ fields }) => {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const classes = style();
   const [formFields, setFormFields] = useState(fields);
-  const { setLoading, setSnackbarConfig } = useContext(AuthContext);
   const [adressErrorCheckBox, setAdressErrorCheckBox] = useState({
     show: false,
     checked: false,
@@ -28,62 +21,6 @@ const StudentInfo = ({ fields }) => {
     if (buttonKey === "save") {
       const result = checkFormValid(formFields);
       setFormFields(result.fields);
-      if (result.isFormValid) {
-        if (!adressErrorCheckBox.checked && constant.verifyAddress) {
-          const countryValue = _.find(
-            fields,
-            (field) => field.key === "country"
-          );
-          const postCodeValue = _.find(
-            fields,
-            (field) => field.key === "postCode"
-          );
-          const streetValue = _.find(fields, (field) => field.key === "street");
-          const streetNumberValue = _.find(
-            fields,
-            (field) => field.key === "number"
-          );
-          const cityValue = _.find(fields, (field) => field.key === "city");
-          // const addressValid = isAddressValid({
-          //   country: [countryValue.value],
-          //   postalCode: [postCodeValue.value],
-          //   addressLines: [
-          //     streetValue.value,
-          //     streetNumberValue.value,
-          //     cityValue.value,
-          //   ],
-          // });
-          // if (!addressValid) {
-          //   setAdressErrorCheckBox({
-          //     ...adressErrorCheckBox,
-          //     show: true,
-          //     checked: false,
-          //   });
-          //   return;
-          // }
-        }
-        //   const payload = getPayloadObj(result.fields);
-        //   try {
-        //     setLoading(true);
-        //     const response = await auth.register({
-        //       ...payload,
-        //       role: "PATIENT",
-        //       lang: getUserLocale(),
-        //     });
-        //     if (response.data.message) {
-        //       setLoading(false);
-        //       setSnackbarConfig(
-        //         handleSnackBar("fire", t("REGISTRATION_SUCCESSFUL"), "success")
-        //       );
-        //       navigate("/login");
-        //     }
-        //   } catch (error) {
-        //     setLoading(false);
-        //     setSnackbarConfig(
-        //       handleSnackBar("fire", error.error || "Api error", "error")
-        //     );
-        //   }
-      }
     } else if (buttonKey === "cancel") {
       navigate("/login");
     }
@@ -94,10 +31,7 @@ const StudentInfo = ({ fields }) => {
     updatedFormFields[index].value = value;
     setFormFields(updatedFormFields);
   };
-  // calling api
 
-  // const payload = {};
-  // const response = await auth.register({ ...payload});
   return (
     <Card>
       <CardContent>
